24a25f69f5d2538bd24be27039b5c357b9237dd3
Deltoidea <codyiorda@gmail.com>
2020-10-04 21:53:17 -0600

    Ugly plot update

--- Assignments/Assignment_6/ugly_plot.R
+++ Assignments/Assignment_6/ugly_plot.R
@@ -11,18 +11,18 @@ names(df)
 
 df$img <-"./Images/sleep.png"
 
-ggplot(df,aes(y=sleep_total,x=brainwt))+geom_point()+
-  geom_image(aes(image=df$img))+
+ggplot(df,aes(y=sleep_total,x=brainwt,color=vore,Image=img))+geom_point()+
+  geom_image(aes(image=df$img),size=.5)+
   geom_smooth(method = lm)+
   facet_wrap(~conservation)+
   labs(title = "Sleepy Animals",x="brain mass",y="total sleep")+
-  theme(text = element_text(family ="Cambria"))+
-  theme(plot.background = element_rect(fill="red"),plot.title = element_text(angle = 355,colour = "lemonchiffon1",hjust =1.0,vjust = .8),panel.background = element_rect(fill="#a64c0c"))+
-  scale_x_continuous(breaks=c(0, 15,23,34, 76, 75, 150))+scale_y_continuous(breaks=c(0, 50, 65, 75, 150,166,202,303,404))+
-  theme(strip.text.x = element_text(size=11, angle=75),
-                                                                           strip.text.y = element_text(size=12, face="italic"),
+  theme(text = element_text(family ="Cambria"),axis.text = element_text(family = "Palatino",angle = 90))+
+  theme(plot.background = element_rect(fill="red"),plot.title = element_text(angle = 355,colour = "lemonchiffon1",hjust =1.0,vjust = .8),panel.background = element_rect(fill="#a64c0c"))+  
+    theme(strip.text.x = element_text(size=7, angle=20),
+                                                                           strip.text.y = element_text(size=12, face="italic",angle = 270),
                                                                            strip.background = element_rect(colour="purple", fill="gold"),element_line(size =2))
 
-
+ggsave("./uglyplot.png",height = 10,width = 5
+)
 ?msleep
 ??ggimage
